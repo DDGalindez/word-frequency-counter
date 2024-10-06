955838f186b5b87ebd0c5c0935255e4f36231f2b
DDGalindez <142036344+DDGalindez@users.noreply.github.com>
2024-10-07 00:50:12 +0800

    count the frequency of each word

--- process.php
+++ process.php
@@ -3,9 +3,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $text = $_POST['text'];
     $sortOrder = $_POST['sort']; 
     $limit = isset($_POST['limit']) ? (int)$_POST['limit'] : 10; 
-    
+ 
     $text = strtolower(preg_replace("/[^\w\s]/", "", $text));
+
+     $words = explode(" ", $text);
     
+     
+     $wordFrequency = array_count_values($words);
+     
+     
 
     
 }
